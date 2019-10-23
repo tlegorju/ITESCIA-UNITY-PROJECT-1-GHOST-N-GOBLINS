e9922ab75c77b2a116c32d50e772f1fd1c10f4e1
tlegorju <thibault.legorju@gmail.com>
2019-10-23 11:54:50 +0200

    Starting project
    Placeholder model for character
    default translating script

---
+++ ITESCIA-UNITY-PROJECT-1-GHOST-N-GOBLINS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_TranslationSpeed;
+    //[SerializeField] float m_RotationSpeed;
+    Transform m_Transform;
+    Rigidbody m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Transform = transform;
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Transform.forward * m_TranslationSpeed * Time.fixedDeltaTime * vInput);
+        //m_Rigidbody.MoveRotation(m_Rigidbody.rotation * Quaternion.AngleAxis(m_RotationSpeed * Time.fixedDeltaTime * hInput, m_Transform.up));
+    }
+}
