5619a91df6e471c3a02c2c1b48fc6ee8624db106
tlegorju <thibault.legorju@gmail.com>
2019-11-06 16:27:36 +0100

    Tir projectile + mort des monstres + mort du player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float lifePoint = 2.0f;
+    private bool canTakeDamages=true;
+
     [SerializeField] float m_TranslationSpeed;
     [SerializeField] float m_GravityForce;
     //[SerializeField] float m_RotationSpeed;
@@ -15,11 +18,25 @@ public class PlayerController : MonoBehaviour
     float m_CapsuleHeight;
     bool isStanding = true;
 
+    bool lookingRight = true;
+
+    ///WEAPON HANDLING
+    [SerializeField] GameObject weaponPrefab;
+    [SerializeField] Transform firePoint;
+    [SerializeField] float fireRate=.25f;
+    private float timeNextThrow;
+    [SerializeField] float throwForce = 1000.0f;
+    [SerializeField] float angleThrowInRadian = 0.3f;
+    ///WEAPON HANDLING
+
+
     private void Awake()
     {
         m_Transform = transform;
         m_Rigidbody = GetComponent<Rigidbody>();
         m_CapsuleCollider = GetComponent<CapsuleCollider>();
+
+        timeNextThrow = Time.time;
     }
     // Start is called before the first frame update
     void Start()
@@ -29,7 +46,7 @@ public class PlayerController : MonoBehaviour
     }
 
     public bool IsGrounded() {
-        return Physics.Raycast(transform.position, -Vector3.up, /*distToGround +*/ 0.1f);
+        return Physics.Raycast(transform.position, -Vector3.up, 0.1f);
     }
 
     private void Update()
@@ -52,6 +69,10 @@ public class PlayerController : MonoBehaviour
             m_CapsuleCollider.center = new Vector3(0, 1, 0);
             isStanding = true;
         }
+        if(Input.GetButtonDown("Fire1") && Time.time >= timeNextThrow)
+        {
+            FireProjectile();
+        }
     }
     
     // Update is called once per frame
@@ -67,6 +88,67 @@ public class PlayerController : MonoBehaviour
             m_Rigidbody.AddForce(Physics.gravity * 2*(m_Rigidbody.mass * m_Rigidbody.mass));
         }
 
+        UpdateFacingDirection();
         //m_Rigidbody.MoveRotation(m_Rigidbody.rotation * Quaternion.AngleAxis(m_RotationSpeed * Time.fixedDeltaTime * hInput, m_Transform.up));
+    }
+
+    private void FireProjectile()
+    {
+        GameObject tmp = Instantiate(weaponPrefab, firePoint.position, Quaternion.identity) as GameObject;
+        if(lookingRight)
+        {
+            tmp.GetComponent<Rigidbody>().AddForce(throwForce * new Vector3(0, Mathf.Sin(angleThrowInRadian), Mathf.Cos(angleThrowInRadian)));
+        }
+        else
+        {
+            tmp.GetComponent<Rigidbody>().AddForce(throwForce * new Vector3(0, Mathf.Sin(angleThrowInRadian), -Mathf.Cos(angleThrowInRadian)));
+        }
+        timeNextThrow = Time.time + fireRate;
+    }
+
+    private void UpdateFacingDirection()
+    {
+        float hInputRaw = Input.GetAxisRaw("Horizontal");
+        if (hInputRaw > 0)
+            lookingRight = true;
+        else if(hInputRaw < 0)
+            lookingRight = false;
+    }
+
+    public void TakeDamages(float damages)
+    {
+        if (!canTakeDamages)
+            return;
+
+        lifePoint -= damages;
+        if (lifePoint <= 0)
+            Dies();
+
+        StartCoroutine("FlashOnDamages");
+    }
+
+    private void Dies()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator FlashOnDamages()
+    {
+        canTakeDamages = false;
+        int flashingCount = 8;
+        float flashingInterval = .1f;
+        bool displayModel = true;
+
+        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+
+        for (int counter = 0; counter < flashingCount; counter++)
+        {
+            displayModel = !displayModel;
+            for (int i = 0; i < renderers.Length; i++)
+                renderers[i].enabled = displayModel;
+
+            yield return new WaitForSeconds(flashingInterval);
+        }
+        canTakeDamages = true;
     }
 }
