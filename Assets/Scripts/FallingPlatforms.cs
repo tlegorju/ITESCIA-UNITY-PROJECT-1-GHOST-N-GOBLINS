1ca60a0be3dbeb57e06f7042ae059f2c5a6362fb
sulliman abdul <sulliman.abdul@gmail.com>
2019-11-06 16:24:51 +0100

    ajout script et animation falling platform

---
+++ Assets/Scripts/FallingPlatforms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatforms : MonoBehaviour
+{
+    [SerializeField] float timeBeforeActivation;
+    [SerializeField] float fallSpeed;
+
+    Vector3 originPosition;
+    Transform m_transform;
+    Rigidbody m_Rigibody;
+
+    void Awake()
+    {
+        m_transform = transform;
+        m_Rigibody = GetComponent<Rigidbody>();
+        originPosition = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(activatePlatform());
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        StopCoroutine(activatePlatform());
+    }
+    IEnumerator activatePlatform()
+    {
+        
+        yield return new WaitForSeconds(timeBeforeActivation);
+        m_Rigibody.useGravity = true;
+        m_Rigibody.isKinematic = false;
+    }
+
+}
