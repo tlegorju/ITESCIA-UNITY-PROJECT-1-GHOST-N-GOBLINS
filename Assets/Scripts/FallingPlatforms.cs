6d26af630de5ce8fcc7660807ddc6958560ed082
sulliman abdul <sulliman.abdul@gmail.com>
2019-11-13 16:53:01 +0100

    ajout falling platform

--- Assets/Scripts/FallingPlatforms.cs
+++ Assets/Scripts/FallingPlatforms.cs
@@ -18,22 +18,54 @@ public class FallingPlatforms : MonoBehaviour
         m_Rigibody = GetComponent<Rigidbody>();
         originPosition = transform.position;
     }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
-        StartCoroutine(activatePlatform());
+
+        if (collision.gameObject.layer.Equals(9))
+        {
+            StartCoroutine(ActivatePlatform());
+        }
+      
+        
     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        StopCoroutine(activatePlatform());
-    }
-    IEnumerator activatePlatform()
+    
+
+    IEnumerator ActivatePlatform()
     {
         
         yield return new WaitForSeconds(timeBeforeActivation);
+        Debug.Log("Activation");
         m_Rigibody.useGravity = true;
         m_Rigibody.isKinematic = false;
+         m_Rigibody.AddForce(new Vector3(0, -fallSpeed, 0));
+        yield return null;
+
     }
 
+    IEnumerator ResetPlatform()
+    {
+        yield return new WaitForSeconds(timeBeforeActivation);
+        Debug.LogError("ResetPlatform");
+        m_Rigibody.isKinematic = true;
+        m_Rigibody.useGravity = false;
+        m_Rigibody.MovePosition(originPosition+ m_transform.up*fallSpeed*Time.deltaTime);
+        StopAllCoroutines();
+        yield return null;
+    }
+
+    public bool IsAboveGround()
+    {
+        return transform.position.y >= 0;
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(IsAboveGround());
+        if (!IsAboveGround())
+        {
+           
+            StartCoroutine(ResetPlatform());
+        }
+    }
 }
