5a315cdc3e8ce3c1c929a86b886de613a9c947c8
tlegorju <thibault.legorju@gmail.com>
2019-12-16 17:25:46 +0100

    texturage armor + Spear + spawner ennemi

---
+++ Assets/Scripts/EnnemiSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class EnnemiSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject ennemiToSpawnPrefab;
+    [SerializeField] Transform spawnPoint;
+    GameObject ennemiSpawned = null;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.transform.position.z >= transform.position.z)
+                return;
+
+            if (ennemiSpawned)
+                return;
+
+            ennemiSpawned = Instantiate(ennemiToSpawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        }
+    }
+}
