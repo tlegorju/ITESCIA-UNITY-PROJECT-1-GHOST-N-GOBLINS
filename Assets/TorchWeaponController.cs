fb8eb92cfe1017c1635f3ee1f40a8a1f61c43216
sulliman abdul <sulliman.abdul@gmail.com>
2020-01-16 09:03:27 +0100

    added torch's functionnalities

---
+++ Assets/TorchWeaponController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchWeaponController : WeaponController
+{
+    // Start is called before the first frame update
+    public float throwSpeed;
+    public float rotateSpeed;
+    protected override void Start()
+    {
+        base.Start();
+        GetComponent<Rigidbody>().velocity = throwSpeed/2 * transform.up+ throwSpeed*transform.forward;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(rotateSpeed*10 * Time.deltaTime, 0, 0));
+    }
+}
