a258b0e20cb8bd3326f7bfea87ed448981411092
tlegorju <thibault.legorju@gmail.com>
2020-01-16 10:01:15 +0100

    creation collectible + reorganisation dossiers

---
+++ Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Collectible : MonoBehaviour
+{
+    [SerializeField] int nbPointsOnCollected;
+
+
+    private void Update()
+    {
+        transform.Rotate(0, Time.deltaTime * 10, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            CollidedWithPlayer(other.gameObject);
+    }
+
+    protected virtual void CollidedWithPlayer(GameObject player)
+    {
+        if (ScoreManager.Instance)
+        {
+            ScoreManager.Instance.AddScore(nbPointsOnCollected);
+        }
+        DestroyObject();
+    }
+
+    protected virtual void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
