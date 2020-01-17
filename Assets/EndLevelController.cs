ad3b9b8d879db5548c3d0da5b07cce850abffdbc
tlegorju <thibault.legorju@gmail.com>
2020-01-17 11:42:09 +0100

    added end level door + collectibles&ennemies in level2

---
+++ Assets/EndLevelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Win");
+            //Load next level
+        }
+    }
+}
