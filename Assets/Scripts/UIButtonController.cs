ee196c76f20c38d7ff20f27b403c4f6c9eae1c2c
SullimanAbdul <SullimanAbdul@users.noreply.github.com>
2020-01-19 14:55:03 +0100

    Finished all the minor changes

--- Assets/Scripts/UIButtonController.cs
+++ Assets/Scripts/UIButtonController.cs
@@ -12,21 +12,20 @@ public class UIButtonController : MonoBehaviour
 
     public void ResumeGame()
     {
-        Debug.Log("OUI");
+        
         if (GameManager.Instance)
             GameManager.Instance.ResumeGame();
     }
 
     public void QuitLevel()
     {
-        Debug.Log("NON");
         if (GameManager.Instance)
             GameManager.Instance.QuitGame();
     }
 
     public void QuitApplication()
     {
-        Debug.Log("POUET");
+        
         Application.Quit();
     }
 }
